6a03bde23ca30801e154b1444a8ebb8a7d5cd71a
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-21 11:02:33 -0600

    test 02

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,13 @@
-function SumArray (arr, n) {
+function SumArray(arr, n) {
   // Your code here:
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === n) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-module.exports = SumArray
+module.exports = SumArray;
