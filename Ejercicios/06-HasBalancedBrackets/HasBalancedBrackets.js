a9e26e357bf3c04cb37b59f30cdb7df4e2c04e0d
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 10:32:34 -0600

    hasBalancedBrakets

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,25 @@
-function HasBalancedBrackets (string) {
-  // Your code here:
+function HasBalancedBrackets(string) {
+  const stack = [];
+  const openingBrackets = ["(", "[", "{"];
+  const closingBrackets = [")", "]", "}"];
+  const bracketPairs = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
 
+  for (const char of string) {
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      const lastOpeningBracket = stack.pop();
+      if (lastOpeningBracket !== bracketPairs[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
 
-module.exports = HasBalancedBrackets
+module.exports = HasBalancedBrackets;
