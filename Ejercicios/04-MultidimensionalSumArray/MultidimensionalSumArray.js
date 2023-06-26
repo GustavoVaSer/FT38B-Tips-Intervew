af158665eb759a83482b0ad5fb46baa46961c872
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 10:18:39 -0600

    multidimensional array

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,20 @@
-function mdArraySum (arr) {
+function mdArraySum(arr) {
   // Your code here:
+  let sum = 0;
 
+  function sumArray(array) {
+    for (let element of array) {
+      if (Array.isArray(element)) {
+        sumArray(element); // Llamada recursiva si el elemento es un arreglo
+      } else {
+        sum += element; // Sumar el elemento al total
+      }
+    }
+  }
+
+  sumArray(arr); // Iniciar la suma con el arreglo dado
+
+  return sum;
 }
 
-module.exports = mdArraySum
+module.exports = mdArraySum;
--- Ejercicios/12-Intersection/Intersection.js
+++ Ejercicios/12-Intersection/Intersection.js
@@ -1,6 +1,9 @@
 function intersection (arr1, arr2) {
   // Your code here:
-
+const result = [];
+ for (const num1 of arr1) {
+  for (const num2 of arr2)
+ }
 }
 
 module.exports = intersection
