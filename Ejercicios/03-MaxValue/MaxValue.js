a0a8f3ee1a737cb39c652798e61f7e7a4f8d6186
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 10:06:39 -0600

    Max Value

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,26 @@
-function MaxValue (shares) {
+function MaxValue(shares) {
   // Your code here:
+  let minPrice = shares[0]; // Precio mínimo inicialmente
+  let maxProfit = 0; // Ganancia máxima inicialmente
 
+  for (let i = 1; i < shares.length; i++) {
+    const currentPrice = shares[i];
+
+    // Actualizar el precio mínimo de compra si encontramos un valor menor
+    if (currentPrice < minPrice) {
+      minPrice = currentPrice;
+    }
+
+    // Calcular la ganancia potencial si vendemos en el precio actual
+    const potentialProfit = currentPrice - minPrice;
+
+    // Actualizar la ganancia máxima si encontramos una ganancia mayor
+    if (potentialProfit > maxProfit) {
+      maxProfit = potentialProfit;
+    }
+  }
+
+  return maxProfit;
 }
 
-module.exports = MaxValue
+module.exports = MaxValue;
