53984db009edea065732043307e7d597b137b974
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 10:37:31 -0600

    Solve Graph

--- Ejercicios/07-SolveGraph/SolveGraph.js
+++ Ejercicios/07-SolveGraph/SolveGraph.js
@@ -1,6 +1,27 @@
-function SolveGraph (graph, start, end) {
-  // Your code here:
+function SolveGraph(graph, start, end) {
+  const visited = new Set();
 
+  function dfs(current) {
+    visited.add(current);
+
+    if (current === end) {
+      return true;
+    }
+
+    const neighbors = graph[current];
+
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        if (dfs(neighbor)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  return dfs(start);
 }
 
-module.exports = SolveGraph
+module.exports = SolveGraph;
