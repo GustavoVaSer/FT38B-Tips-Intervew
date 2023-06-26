d4565e84461224ec390ad0c339c9a9032bd7bdd1
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 10:42:39 -0600

    BinaryToDecimal

--- Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
@@ -1,6 +1,15 @@
-function FindWordStartingWith (book, query) {
-  // Your code here:
+function FindWordStartingWith(book, query) {
+  const words = book.text.split(" ");
+  const result = [];
 
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i].toLowerCase();
+    if (word.startsWith(query.toLowerCase())) {
+      result.push(i);
+    }
+  }
+
+  return result;
 }
 
-module.exports = FindWordStartingWith
+module.exports = FindWordStartingWith;
--- Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
+++ Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
@@ -1,6 +1,13 @@
-function BinaryToDecimal (binary) {
-  // Your code here:
+function BinaryToDecimal(binary) {
+  let decimal = 0;
 
+  for (let i = binary.length - 1; i >= 0; i--) {
+    const bit = parseInt(binary[i]);
+    const power = binary.length - 1 - i;
+    decimal += bit * Math.pow(2, power);
+  }
+
+  return decimal;
 }
 
-module.exports = BinaryToDecimal
+module.exports = BinaryToDecimal;
